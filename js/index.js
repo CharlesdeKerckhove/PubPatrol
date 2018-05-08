dd6d0518509071c53bf85117bb75df3f7b41cdf3
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2018-05-08 06:20:34 +0100

    idk

--- js/index.js
+++ js/index.js
@@ -1,83 +1,115 @@
-//Creating the Map
-var map;
-var information;
-var marker;
-var service;
-var currentLocation = {lat: 52.19544 , lng: -2.22387};
-//Check for geolocation data
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var currentLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        information.open(map);
-        map.setCenter(currentLocation);
-        
-    },
-        function() {
-            handleLocationError(true, information, map.getCenter());
-        });
-        } else {
-            information.open(map);
-            map.setCenter(currentLocation);
-        // If the browser doesn't allow Geolocation
-        handleLocationError(false, information, map.getCenter());
-        } 
-function initMap() {
-    information = new google.maps.InfoWindow;
-    
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: currentLocation,
-      zoom: 16
-    }); 
-    
-    marker = new google.maps.Marker({
-      position: currentLocation,
-      map: map
-    });
-    
-    var request = {
-    location: currentLocation,
-    radius: '300',
-    query: 'pub'
+document.addEventListener("deviceready", getPlacesLocation, false);
+var Map;
+var Infowindow;
+var Latitude = undefined;
+var Longitude = undefined;
+ 
+// Get geo coordinates
+ 
+function getPlacesLocation() {
+    navigator.geolocation.getCurrentPosition
+    (onPlacesSuccess, onPlacesError, { enableHighAccuracy: true });
+}
+ 
+// Success callback for get geo coordinates
+ 
+var onPlacesSuccess = function (position) {
+ 
+    Latitude = position.coords.latitude;
+    Longitude = position.coords.longitude;
+ 
+    getPlaces(Latitude, Longitude);
+ 
+}
+ 
+// Get places by using coordinates
+ 
+function getPlaces(latitude, longitude) {
+ 
+    var latLong = new google.maps.LatLng(latitude, longitude);
+ 
+    var mapOptions = {
+ 
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+ 
     };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-
-    
+ 
+    Map = new google.maps.Map(document.getElementById("places"), mapOptions);
+ 
+    Infowindow = new google.maps.InfoWindow();
+ 
+    var service = new google.maps.places.PlacesService(Map);
+    service.nearbySearch({
+ 
+        location: latLong,
+        radius: 500,
+        type: ['store']
+    }, foundStoresCallback);
+ 
 }
-
-function handleLocationError(browserHasGeolocation, information, currentLocation) {
-    information.setPosition(currentLocation);
-    information.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser does not allow geolocation.');
-    information.open(map);
-}
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-        console.log(place);
-      createMarker(results[i]);
+ 
+// Success callback for watching your changing position
+ 
+var onPlacesWatchSuccess = function (position) {
+ 
+    var updatedLatitude = position.coords.latitude;
+    var updatedLongitude = position.coords.longitude;
+ 
+    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+ 
+        Latitude = updatedLatitude;
+        Longitude = updatedLongitude;
+ 
+        getPlaces(updatedLatitude, updatedLongitude);
     }
-  }
 }
-function createMarker(place){
-    var placesList = document.getElementById("places");
-
+ 
+// Success callback for locating stores in the area
+ 
+function foundStoresCallback(results, status) {
+ 
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+ 
+        for (var i = 0; i < results.length; i++) {
+ 
+            createMarker(results[i]);
+ 
+        }
+    }
+}
+ 
+// Place a pin for each store on the map
+ 
+function createMarker(place) {
+ 
+    var placeLoc = place.geometry.location;
+ 
     var marker = new google.maps.Marker({
-        map: map,
-        icon: {
-            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-            strokeColor: 'blue',
-            scale: 5
-        },
-        position: place.geometry.location 
-    })
-    var li = document.createElement('li');
-          li.textContent = place.name;
-          placesList.appendChild(li);
+        map: Map,
+        position: place.geometry.location
+    });
+ 
+    google.maps.event.addListener(marker, 'click', function () {
+ 
+        Infowindow.setContent(place.name);
+        Infowindow.open(Map, this);
+ 
+    });
+}
+ 
+// Error callback
+ 
+function onPlacesError(error) {
+    console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
+ 
+// Watch your changing position
+ 
+function watchPlacesPosition() {
+ 
+    return navigator.geolocation.watchPosition
+    (onPlacesWatchSuccess, onPlacesError, { enableHighAccuracy: true });
 }
